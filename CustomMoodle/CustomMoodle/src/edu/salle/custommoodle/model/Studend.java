e9f0adc2cd2723af1f82dceffcac0cbe7f263fde
Carolina <deniu_10MR@hotmail.com>
2018-09-03 20:11:17 -0500

    Segundo Cambio
    se agrega el botón de search y refresh, la tabla también

---
+++ CustomMoodle/CustomMoodle/src/edu/salle/custommoodle/model/Studend.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.salle.custommoodle.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author ASUS
+ */
+//public class Studend implements Comparable
+public class Studend 
+{
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Studend(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+    }
+    
+    private String name;
+    private String lastName;
+    private String id;
+    
+ 
+
+    @Override
+    public String toString() {
+        return String.format("id=%s,\n name=%s,\n lastName=%s",id,name,lastName); //To change body of generated methods, choose Tools | Templates.
+    }
+
+//    @Override
+//    public int compareTo(Object o) {
+////        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//          return ((Studend)o).id.compareTo(id);
+//    }
+//    
+//    @Override
+//    public int hashCode()
+//    {
+//        return id.hashCode();
+//    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Studend other = (Studend) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+     
+    
+    
+}
